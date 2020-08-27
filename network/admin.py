e4a07ec83d133eeee66e75c702dd15556d62a7ab
SHorne41 <stephane.horne@gmail.com>
2020-08-27 13:18:09 -0400

    Added Profile and Following models to models.py; registered to admin site in admin.py

--- network/admin.py
+++ network/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Post
+from .models import User, Post, Profile, Following
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Following)
--- network/models.py
+++ network/models.py
@@ -6,7 +6,16 @@ class User(AbstractUser):
     pass
 
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length = 280)
     likes = models.IntegerField(default = 0)
     timestamp = models.DateTimeField()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    numFollowers = models.IntegerField(default = 0)
+    numFollowing = models.IntegerField(default = 0)
+
+class Following(models.Model):
+    followingUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    followedUser = models.ForeignKey(User, on_delete=models.CASCADE)
